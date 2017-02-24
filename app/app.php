e5e92c9a688fcc2a3f4d71c42d48eebaadbb2115
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-02-24 11:12:27 -0800

    add Sylex routes in app.php.

--- app/app.php
+++ app/app.php
@@ -2,8 +2,8 @@
 
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Student.php";
-    require_once __DIR__."/../src/Teacher.php";
+    require_once __DIR__."/../src/Client.php";
+    require_once __DIR__."/../src/Stylist.php";
 
     use Symfony\Component\Debug\Debug;
     Debug::enable();
@@ -23,7 +23,37 @@
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
-    $app->get("/")
+    $app->get("/", function() use ($app) {
+
+        return $app['twig']->render('index.html.twig');
+
+    });
+
+    $app->get("/Stylists", function() use ($app) {
+
+        return $app['twig']->render('stylists.html.twig', array('stylists'=>Stylist::getAll()));
+
+    });
+    $app->post("/Stylists", function() use ($app) {
+
+        $new_stylist = new Stylist($_POST['stylist_name']);
+        $new_stylist->save();
+        
+        return $app['twig']->render('stylists.html.twig', array('stylists'=>Stylist::getAll()));
+
+    });
+
+    $app->get("/Stylists/{id}", function($id) use ($app) {
+
+        $stylist = Stylist::findStylist($id);
+
+        return $app['twig']->render('stylist.html.twig', array('stylist'=>$stylist));
+
+    })
+
+
+
+
 
 
 
